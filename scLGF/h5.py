0ec344de13af5a46afcdb89943d53169890a4e33
lanyangyang <78328034+lijing2000@users.noreply.github.com>
2024-09-04 20:42:47 +0800

    Delete scLGF/h5.py

--- scLGF/h5.py
+++
@@ -1,9 +0,0 @@
-import h5py
-
-with h5py.File('data/Pollen.h5', "r") as f:
-    for key in f.keys():
-        print(f[key], key, f[key].name)
-
-    d4_group = f["var"]
-    for key in d4_group.keys():
-        print(key, d4_group[key], d4_group[key].name, d4_group[key].value);
